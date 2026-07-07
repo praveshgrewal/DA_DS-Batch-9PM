fa81742c5cafd9e32f7fab1be9c9360d436f4434
praveshgrewal <gurahpravesh@gmail.com>
2026-07-07 20:38:00 +0530

    while loop

--- session1.py
+++ session1.py
@@ -1,4 +1,17 @@
-print("hii my self Pravesh Grewal.")
-print("Hello World.")
-# this is comment
-12+4
+# print("hii my self Pravesh Grewal.")
+# print("Hello World.")
+# # this is comment
+# 12+4
+
+
+for i in range(1,11):
+    pass
+
+
+
+num = 23
+
+if num == 0:
+    print("Number is zero")
+else:
+    print("Number is not zero")
